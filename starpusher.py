11e6372e0fe875b46bd227ae3fd1eeb7238384f7
Elliott Hauser <eah13@mac.com>
2013-11-11 08:44:13 -0500

    Made tiny window

--- starpusher.py
+++ starpusher.py
@@ -7,8 +7,8 @@ import random, sys, copy, os, pygame
 from pygame.locals import *
 
 FPS = 30 # frames per second to update the screen
-WINWIDTH = 800 # width of the program's window, in pixels
-WINHEIGHT = 600 # height in pixels
+WINWIDTH = 80 # width of the program's window, in pixels
+WINHEIGHT = 60 # height in pixels
 HALF_WINWIDTH = int(WINWIDTH / 2)
 HALF_WINHEIGHT = int(WINHEIGHT / 2)
 
